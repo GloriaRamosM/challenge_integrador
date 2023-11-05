0c87461e1722937353f1d3f8d0199b96806dfb0b
GloriaRamosM <gloriaramosmoran@gmail.com>
2023-11-05 11:49:29 -0300

    mission 4 terminada, con servidor dinamico, y las rutas creadas

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const mainRoutes = require("./src/routes/mainRoutes");
+const shopRoutes = require("./src/routes/shopRoutes");
+const adminRoutes = require("./src/routes/adminRoutes");
+const authRoutes = require("./src/routes/authRoutes");
+
+app.use(express.static("public"));
+
+app.use("/", mainRoutes);
+app.use("/shop", shopRoutes);
+app.use("/admin", adminRoutes);
+app.use("/admin", authRoutes);
+
+app.listen(4500, () =>
+  console.log("Servidor corriendo en http://localhost:4500")
+);
