04c2de1761a0b171ca4d5da6101a0ac88144d057
GloriaRamosM <gloriaramosmoran@gmail.com>
2023-12-01 11:16:44 -0300

    agregue un formulario

---
+++ public/js/formulario.js
@@ -0,0 +1,15 @@
+const submitForm = (event) => {
+  event.preventDefault();
+
+  // Obtener los valores del formulario
+  let nombre = document.getElementById("nombre").value;
+  let email = document.getElementById("email").value;
+  let mensaje = document.getElementById("mensaje").value;
+
+  // Aquí puedes realizar acciones con los datos, como enviarlos a un servidor o mostrarlos en la consola
+  console.log("Nombre: " + nombre);
+  console.log("Email: " + email);
+  console.log("Mensaje: " + mensaje);
+
+  // Puedes agregar aquí el código para enviar los datos a un servidor mediante AJAX, por ejemplo.
+};
