fe3d3255306f06192fd8615c4b74cb557e57a934
GloriaRamosM <gloriaramosmoran@gmail.com>
2023-12-19 20:21:11 -0300

    agregue multer

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const rout = express.Router();
+const multer = require("multer");
+const path = require("path");
+
 const {
   admin,
   createView,
@@ -9,9 +12,19 @@ const {
   deleteItem,
 } = require("../controllers/adminControllers");
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) =>
+    cb(null, path.resolve(__dirname, "../../public/img/multimedia/products")),
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
 rout.get("/", admin);
 rout.get("/create", createView);
-rout.post("/create", createItem);
+rout.post("/create", upload.array("images", 2), createItem);
 rout.get("/edit/:id", editView);
 rout.put("/edit/:id", editItem);
 rout.delete("/delete/:id", deleteItem);
