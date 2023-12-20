ba1fc31b9a4c1ca45bb808bfc1ce76a8681e4544
GloriaRamosM <gloriaramosmoran@gmail.com>
2023-12-19 21:45:27 -0300

    create funcionando y guardandose en la base de datros

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const { getAll, getOne } = require("../models/product.model");
+const { getAll, getOne, create } = require("../models/product.model");
 
 const adminControllers = {
   admin: async (req, res) => {
@@ -9,9 +9,30 @@ const adminControllers = {
       data,
     });
   },
+
   createView: (req, res) =>
     res.render(path.resolve(__dirname, "../views/admin/create.ejs")),
-  createItem: (req, res) => res.send("Route for admin/create view pero post"),
+
+  createItem: async (req, res) => {
+    const product_schema = {
+      product_name: req.body.name,
+      product_description: req.body.description,
+      price: Number(req.body.price),
+      stock: Number(req.body.stock),
+      discount: Number(req.body.discount),
+      sku: req.body.sku,
+      dues: Number(req.body.dues),
+      img_front: req.files[0].originalname,
+      img_back: req.files[1].originalname,
+      licence_id: Number(req.body.licence),
+      category_id: Number(req.body.category),
+    };
+
+    await create([Object.values(product_schema)]);
+
+    res.redirect("/admin");
+  },
+
   editView: async (req, res) => {
     const { id } = req.params;
     const [product] = await getOne({ product_id: id });
@@ -21,6 +42,7 @@ const adminControllers = {
     });
   },
   editItem: (req, res) => res.send("Route for admin/edit/:id pero put"),
+
   deleteItem: (req, res) => res.send("Route for admin delete id view"),
 };
 
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -27,7 +27,23 @@ const getOne = async (param) => {
   }
 };
 
+const create = async (params) => {
+  try {
+    const [product] = await conn.query(
+      "INSERT INTO product (product_name, product_description, price, stock, discount, sku, dues, img_front, img_back, licence_id, category_id) VALUES ? ;",
+      [params]
+    );
+
+    return product;
+  } catch (e) {
+    return { error: true, message: "Hemos encontrado un error: " + e };
+  } finally {
+    conn.releaseConnection();
+  }
+};
+
 module.exports = {
   getAll,
   getOne,
+  create,
 };
